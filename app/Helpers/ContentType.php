b7435ae98ca1ce9d2e23be041555d6786dc87540
Akrez <akrez.like@gmail.com>
2024-06-05 16:12:56 +0330

    ContentType

--- app/Helpers/ContentType.php
+++ app/Helpers/ContentType.php
@@ -13,4 +13,14 @@ class ContentType
     {
         return trim(preg_replace('@;.*@', '', static::getHeaderContentType($response)));
     }
+
+    public static function isTextCss($response): bool
+    {
+        return boolval(ContentType::getContentType($response) == 'text/css');
+    }
+
+    public static function isTextHtml($response): bool
+    {
+        return boolval(ContentType::getContentType($response) == 'text/html');
+    }
 }
