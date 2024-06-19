f3051ea0a7ce268895507a8709162c1b3969cc42
Akrez <akrez.like@gmail.com>
2024-06-19 21:29:33 +0330

    crypt2

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use App\Exceptions\NotCrypted2Exception;
 use App\Exceptions\NotCrypted3Exception;
 use App\Middlewares\CookieAfterRequest;
 use App\Middlewares\CookieBeforeRequest;
+use App\Middlewares\Crypt2AfterRequest;
+use App\Middlewares\Crypt2BeforeRequest;
 use App\Middlewares\Crypt3AfterRequest;
 use App\Middlewares\Crypt3BeforeRequest;
 use App\Middlewares\Emitter;
@@ -13,26 +15,26 @@ use App\Middlewares\SetAcceptEncoding;
 
 require_once '../vendor/autoload.php';
 
-$baseUrl = Aklon::suggestBaseUrl();
-
-$aklon = new Aklon($baseUrl);
+$aklon = new Aklon();
 
 $request = Aklon::buildRequestFromGlobals();
 
 try {
     $aklon->handle($request, [
-        new Crypt3BeforeRequest,
+        new Crypt2BeforeRequest(123),
         new SetAcceptEncoding,
         new CookieBeforeRequest,
     ], [
         new CookieAfterRequest,
-        new Crypt3AfterRequest,
+        new Crypt2AfterRequest(123),
         new Log,
         new Emitter,
     ]);
 } catch (NotCrypted2Exception $e) {
+    $baseUrl = $aklon->getBaseUrl();
     require_once '../view/form2.php';
 } catch (NotCrypted3Exception $e) {
+    $baseUrl = $aklon->getBaseUrl();
     require_once '../view/form3.php';
 } catch (\Exception $e) {
 }
