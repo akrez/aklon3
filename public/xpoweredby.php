64efad5ecc3b1a3c73ece05dc68eae469db01ae9
Akrez <akrez.like@gmail.com>
2024-06-27 00:57:48 +0330

    xpoweredby

---
+++ public/xpoweredby.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Aklon;
+use App\Exceptions\NotCryptedException;
+use App\Middlewares\Emitter;
+use App\Middlewares\Log;
+use App\Middlewares\SetAcceptEncoding;
+use GuzzleHttp\Psr7\Uri;
+
+require_once '../aklon2.core/config.php';
+require_once BASE_PATH . '/vendor/autoload.php';
+
+try {
+    $aklon = new Aklon();
+
+    $XPoweredBy = implode('', $request->getHeader('X-Powered-By'));
+    if (empty($XPoweredBy)) {
+        throw new NotCryptedException();
+    }
+
+    $request = $request->withoutHeader('X-Powered-By')
+        ->withUri(new Uri($XPoweredBy));
+
+    $aklon->handle($request, [
+        new SetAcceptEncoding,
+    ], [
+        new Log,
+        new Emitter,
+    ]);
+} catch (NotCryptedException $e) {
+} catch (\Exception $e) {
+}
